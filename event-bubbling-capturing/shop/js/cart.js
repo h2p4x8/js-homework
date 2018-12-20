99a24c5829a0899af46399fd3297ce28ebf2aeac
h2p4x8 <44549195+h2p4x8@users.noreply.github.com>
2018-12-20 20:58:36 +0300

    Add files via upload

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const itemList = document.querySelector('.items-list');
+itemList.addEventListener('click', addToCartFunction);
+
+function addToCartFunction(event) {
+  if (!event.target.classList.contains('add-to-cart')) {
+    return;
+  } else {
+    event.preventDefault();
+    var item = {
+      title: event.target.dataset.title,
+      price: event.target.dataset.price
+    }
+    addToCart(item);
+  }
+}
