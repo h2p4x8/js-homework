68396391df2f2fc68cef0904f404cfb9cfc4cbde
h2p4x8 <44549195+h2p4x8@users.noreply.github.com>
2018-12-20 20:59:09 +0300

    Add files via upload

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (!event.target.classList.contains('prop__name')){
+    return;
+  }
+
+  let sortDirection = 1;
+  if (event.target.dataset.dir === '1') {
+      sortDirection = -1;
+  }
+  event.target.dataset.dir = sortDirection;
+  table.dataset.sortBy = event.target.dataset.propName;
+  sortTable(table.dataset.sortBy, sortDirection);
 }
